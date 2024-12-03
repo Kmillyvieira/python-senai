d60bf79bf5edd10815004238ed50a410f2d5d030
Kmillyvieira <Kmillyvieiraa@gmail.com>
2024-12-03 11:32:58 -0300

    add variado
    03/12/24

---
+++ Aula 2/aula02.py
@@ -0,0 +1,11 @@
+print('hi everyone')
+
+nome_usuario ='kemilly vieira' # tipo -> str = string
+idade_usuario = 17 # my age
+altura_usuario = 1.67 # tipo -> float = ponto flutante
+maior_idade = False # tipo -> bool = boolean
+
+print('Welcome', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('você tem', altura_usuario, 'metros.')
+print('você é maior de idade?', maior_idade)
